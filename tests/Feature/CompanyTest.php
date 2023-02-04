3ce8376531f0602fd81193055d198ba63f79a2d9
Ben Gunn <bengunn.dev@gmail.com>
2023-02-04 18:13:58 +0630

    add test setup

--- tests/Feature/CompanyTest.php
+++ tests/Feature/CompanyTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Company\Company;
 use App\Models\User;
+use CompanyTableSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,6 +12,20 @@ use Tests\TestCase;
 class CompanyTest extends TestCase
 {
     use RefreshDatabase;
+
+    private User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = $this->getUser();
+    }
+
+    private function getUser(): User
+    {
+        return User::factory()->create();
+    }
     /**
      * A basic feature test example.
      *
@@ -17,11 +33,23 @@ class CompanyTest extends TestCase
      */
     public function test_company_home_page_contain_empty_data()
     {
-        $user = User::factory()->create();
-
-        $response = $this->actingAs($user)->get('/');
+        $response = $this->actingAs($this->user)->get('/');
 
         $response->assertStatus(200);
         $response->assertSee('No Data Available!');
+    }
+
+    public function test_company_home_page_contain_data()
+    {
+        $this->seed([CompanyTableSeeder::class]);
+
+        $company = Company::latest()->first();
+
+        $response = $this->actingAs($this->user)->get('/');
+
+        $response->assertStatus(200);
+        $response->assertViewHas('companies', function ($collection) use ($company) {
+            return $collection->contains($company);
+        });
     }
 }
